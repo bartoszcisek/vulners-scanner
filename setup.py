606d3e47ed07abf12ba130d12e7b1bdf02c9aa36
Bartosz Cisek <b.cisek@piwik.pro>
2018-05-21 14:23:59 +0200

    py_modules

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/videns/vulners-scanner",
     packages=setuptools.find_packages(),
+    py_modules=["scanModules"],
     scripts=['vulners_scanner/linuxScanner.py', 'vulners_scanner/lazyScanner.py'],
     classifiers=(
         "Programming Language :: Python :: 3",
