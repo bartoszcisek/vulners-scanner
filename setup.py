c7b79cdfab4895bf51319a1c4da22fa02f2b0209
Bartosz Cisek <b.cisek@piwik.pro>
2018-05-21 11:24:17 +0200

    Test pip packaging

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vulners_scanner",
+    version="0.0.1",
+    author="videns",
+    author_email="ibulatenko@gmail.com",
+    description="vulnerability scanner",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/videns/vulners-scanner",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
